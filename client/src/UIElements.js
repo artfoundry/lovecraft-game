1d041f3abac448e2cadd6a903177d2d16fcb5158
David Markowitz <dave@artfoundry.com>
2023-11-21 23:26:00 -0800

    Fixing some minor alignment issues in control bars

--- client/src/UIElements.js
+++ client/src/UIElements.js
@@ -168,8 +168,8 @@ function CharacterControls(props) {
 				className={`character-control-container ${((props.screenData.isNarrow || props.screenData.isShort) && props.characterId !== props.selectedControlTab) ? 'hide' : ''}`}>
 				{props.inTacticalMode &&
 				<div className='control-bar-actions-moves'>
-					<div className='control-bar-actions-moves-title'>Moves: <span className='control-bar-actions-moves-value'>{props.isActiveCharacter ? props.movesRemaining : ''}</span></div>
-					<div className='control-bar-actions-moves-title'>Actions: <span className='control-bar-actions-moves-value'>{props.isActiveCharacter ? props.actionsRemaining : ''}</span></div>
+					<div className='control-bar-actions-moves-title'>Moves: <span className='control-bar-actions-moves-value'>{props.isActiveCharacter ? props.movesRemaining : '0'}</span></div>
+					<div className='control-bar-actions-moves-title'>Actions: <span className='control-bar-actions-moves-value'>{props.isActiveCharacter ? props.actionsRemaining : '0'}</span></div>
 				</div>}
 				{weaponButtons}
 				{medicineButtons}
